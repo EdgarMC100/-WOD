d018cc06cc19905d369e3a945a14a44a2a48b78e
edgarmc100 <edgarmc100@gmail.com>
2022-10-24 15:21:01 -0500

    Adds Route versioning

--- src/index.js
+++ src/index.js
@@ -4,9 +4,7 @@ const v1Router = require("./v1/routes");
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("<h2>It's workings</h2>");
-});
+app.use("/api/v1", v1Router);
 
 app.listen(port, () => {
   console.log(`API is listening on port ${port}`);
