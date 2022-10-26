0dadde6ca9433ef80b2638df5f9fc8a2d0cd5a91
edgarmc100 <edgarmc100@gmail.com>
2022-10-26 12:08:05 -0500

    Adds dummy data using a json file

---
+++ src/services/workoutService.js
@@ -0,0 +1,30 @@
+const workout = require("../database/workout");
+
+const getAllWorkouts = () => {
+  const allWorkouts = workout.getAllWorkouts();
+  return allWorkouts;
+};
+
+const getOneWorkout = () => {
+  return;
+};
+
+const createNewWorkout = () => {
+  return;
+};
+
+const updateOneWorkout = () => {
+  return;
+};
+
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkouts,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
