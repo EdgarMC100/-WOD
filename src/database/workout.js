7998d809305e9eb42dea07f13eaaa35f98a64c73
edgarmc100 <edgarmc100@gmail.com>
2022-10-27 15:03:04 -0500

    Adds functionality for comparing workouts and creating new ones

--- src/database/workout.js
+++ src/database/workout.js
@@ -1,7 +1,20 @@
 const DB = require("./db.json");
+const { saveToDatabase } = require("./utils");
 
 const getAllWorkouts = () => {
   return DB.workouts;
 };
 
-module.exports = { getAllWorkouts };
+const compareWorkouts = (workout, newWorkout) =>
+  workout.name === newWorkout.name;
+
+const createNewWorkout = (newWorkout) => {
+  const isAlreadyAdded = DB.workouts.findIndex(compareWorkouts) > -1;
+  if (isAlreadyAdded) {
+    return;
+  }
+  DB.workouts.push(newWorkout);
+  saveToDatabase(DB);
+  return newWorkout;
+};
+module.exports = { getAllWorkouts, createNewWorkout };
